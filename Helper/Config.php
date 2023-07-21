c56cd29bb3681639b3df6c62390b4d78322cbf9b
Daniel Navarro <danidnm@gmail.com>
2023-07-21 14:24:42 +0200

    Initial commit

---
+++ Helper/Config.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DanielNavarro\ChatGpt\Helper;
+
+class Config extends \Magento\Framework\App\Helper\AbstractHelper
+{
+    const PATH_CHATGPT_ENABLE = 'danielnavarro_chatgpt/general/enable';
+    const PATH_CHATGPT_API_Key = 'farmaciasvivo_stock/general/api_key';
+
+    /**
+     * Check if ChatGPT integration is enabled
+     * @param $store_id
+     * @return mixed
+     */
+    public function isEnabled($store_id = null) {
+        return $this->scopeConfig->getValue(
+            self::PATH_CHATGPT_ENABLE,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $store_id
+        );
+    }
+
+    /**
+     * Returns API Key for the integration
+     * @param $store_id
+     * @return mixed
+     */
+    public function getApiKey($store_id = null) {
+        return $this->scopeConfig->getValue(
+            self::PATH_CHATGPT_API_Key,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $store_id
+        );
+    }
+}
